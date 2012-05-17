fbd92075e5356bd4338cb85fef08d6c26d1beced
Geoffroy Couprie <geo.couprie@gmail.com>
2012-05-17 23:51:20 +0200

    create the bookmarklet

---
+++ src/bookmarklet.js
@@ -0,0 +1,6 @@
+javascript:(function(){
+  _my_script=document.createElement('script');
+  _my_script.type='text/javascript';
+  _my_script.src='http://lolcatho.st/src/unpapered.js?'+Math.random();
+  document.getElementsByTagName('head')[0].appendChild(_my_script);
+})();
