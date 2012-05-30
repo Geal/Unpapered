2134abc881ab0dbad941401ff4a5d1e5567bd775
Geoffroy Couprie <geo.couprie@gmail.com>
2012-05-30 23:27:28 +0200

    add a simple uploader

---
+++ src/build.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+cat remoteStorage-0.5.5.min.js readability.js uploader.js > allscripts.js
---
+++ src/uploader.js
@@ -0,0 +1,25 @@
+var storageInfo = {"type":"pds-remotestorage-00#simple",
+                   "href":"https://5apps.com/storage/geal",
+                   "auth":{"type":"pds-oauth2-00",
+                           "href":"https://5apps.com/oauth/geal"},
+                   "rel":"pds-remotestorage-00#simple"};
+var token = "87de1829c9ddda04e65b50e404b00ce1";
+var user = "geal@5apps.com";
+
+console.log(token);
+var client = remoteStorage.createClient(storageInfo, "tutorial", token);
+
+var value = readability.init(); 
+console.log(value);
+var content = value.content;
+var key = "text";
+client.put(key, content, function(error) {
+  if (error) {
+    //alert('Could not store "' + key + '" in "' + category + '" category');
+    console.log(error);
+  } else {
+    //console.log('Stored "' + value + '" for key "' + key + '" in "' + category + '" category');
+  }
+
+  //callback(error);
+});
