dddf57ca32c7fc301783389ac32a0af4fc172b7d
Geoffroy Couprie <geo.couprie@gmail.com>
2012-06-04 22:34:05 +0200

    use an index to manage the saved pages

--- src/uploader.js
+++ src/uploader.js
@@ -1,25 +1,45 @@
-var storageInfo = {"type":"pds-remotestorage-00#webdav",
-                   "href":"https://owncube.com/apps/remoteStorage/WebDAV.php/geal/remoteStorage",
-                   "auth":{"type":"pds-oauth2-00",
-                           "href":"https://owncube.com/apps/remoteStorage/auth.php/geal"},
-                   "rel":"pds-remotestorage-00#webdav"};
-var token = "cmVtb3RlU3RvcmFnZTo0ZmM3ZTBlNGI2Yjc4";
-var user = "geal@owncube.com";
+var storageInfo = {"type": "pds-remotestorage-00#webdav",
+                   "href": "https://owncube.com/apps/remoteStorage/WebDAV.php/geal/remoteStorage",
+                   "auth": {"type": "pds-oauth2-00",
+                           "href": "https://owncube.com/apps/remoteStorage/auth.php/geal"
+                           }
+                  }
+var token = "cmVtb3RlU3RvcmFnZTo0ZmNkMDI1MmM4Y2I3"
+var user = "geal@owncube.com"
 
 console.log(token);
-var client = remoteStorage.createClient(storageInfo, "tutorial", token);
+var client = remoteStorage.createClient(storageInfo, "unpapered", token);
 
 var value = readability.init(); 
-console.log(value);
-var content = JSON.stringify(value);
-var key = "text";
-client.put(key, content, function(error) {
-  if (error) {
-    //alert('Could not store "' + key + '" in "' + category + '" category');
-    console.log(error);
-  } else {
-    //console.log('Stored "' + value + '" for key "' + key + '" in "' + category + '" category');
-  }
 
-  //callback(error);
+client.get("index", function(error, data) {
+  if(error) {
+    console.log("got an error: "+error)
+  } else {
+    console.log(data)
+
+    var index
+    if(typeof data === "undefined")
+      index = Array()
+    else
+      index = JSON.parse(data)
+
+    index.push(value.title.replace(/<(?:.|\n)*?>/gm, ''))
+    client.put("index", JSON.stringify(index), function(error) {
+      if(error)
+        console.log(error)
+      else {
+        console.log("index updated")
+        var content = JSON.stringify(value)
+        var key = value.title.replace(/<(?:.|\n)*?>/gm, '')
+        client.put(key, content, function(error) {
+          if(error)
+            console.log(error)
+          else
+            console.log("content uploaded")
+        })
+      }
+    })
+  }
 });
+
