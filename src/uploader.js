8f8151ca58c1dcf6f64a8e54251575d8139488f8
Geoffroy Couprie <geo.couprie@gmail.com>
2012-06-05 19:57:10 +0200

    wrap the uploader in a function

--- src/uploader.js
+++ src/uploader.js
@@ -7,39 +7,44 @@ var storageInfo = {"type": "pds-remotestorage-00#webdav",
 var token = "cmVtb3RlU3RvcmFnZTo0ZmNkMDI1MmM4Y2I3"
 var user = "geal@owncube.com"
 
-console.log(token);
-var client = remoteStorage.createClient(storageInfo, "unpapered", token);
 
-var value = readability.init(); 
+function uploader(storageInfo, token, user) {
+  console.log(token)
+  var client = remoteStorage.createClient(storageInfo, "unpapered", token)
 
-client.get("index", function(error, data) {
-  if(error) {
-    console.log("got an error: "+error)
-  } else {
-    console.log(data)
+  var value = readability.init()
 
-    var index
-    if(typeof data === "undefined")
-      index = Array()
-    else
-      index = JSON.parse(data)
+  client.get("index", function(error, data) {
+    if(error) {
+      console.log("got an error: "+error)
+    } else {
+      console.log(data)
 
-    index.push(value.title.replace(/<(?:.|\n)*?>/gm, ''))
-    client.put("index", JSON.stringify(index), function(error) {
-      if(error)
-        console.log(error)
-      else {
-        console.log("index updated")
-        var content = JSON.stringify(value)
-        var key = value.title.replace(/<(?:.|\n)*?>/gm, '')
-        client.put(key, content, function(error) {
-          if(error)
-            console.log(error)
-          else
-            console.log("content uploaded")
-        })
-      }
-    })
-  }
-});
+      var index
+      if(typeof data === "undefined")
+        index = Array()
+      else
+        index = JSON.parse(data)
 
+      index.push(value.title.replace(/<(?:.|\n)*?>/gm, ''))
+      client.put("index", JSON.stringify(index), function(error) {
+        if(error)
+          console.log(error)
+        else {
+          console.log("index updated")
+          var content = JSON.stringify(value)
+          var key = value.title.replace(/<(?:.|\n)*?>/gm, '')
+          client.put(key, content, function(error) {
+            if(error)
+              console.log(error)
+            else
+              console.log("content uploaded")
+          })
+        }
+      })
+    }
+  });
+
+}
+
+uploader(storageInfo, token, user);
