3f5b2a8e20f88a276ca9d0bb2a2946b2a91549b9
Geoffroy Couprie <geo.couprie@gmail.com>
2012-06-09 00:43:32 +0200

    remove dead code

--- src/app.js
+++ src/app.js
@@ -151,56 +151,6 @@
       return bookmarklet;
     }
     $("#bookmarklet").empty().append($("<a>").attr("href", createBookmarklet()).text("Unhost this"))
-    /*$('#publishPublic').on('click', function() {
-      var key = $('#publicKey').val();
-      var value = $('#publicValue').val();
-
-      helper.showSpinner('publishPublicSpinner');
-
-      storage.putData('public', key, value, function(error) {
-        if (!error) {
-          $('#publicValue').val('');
-        }
-
-        helper.hideSpinner('publishPublicSpinner');
-      });
-
-      return false;
-    });
-
-
-    $('#publishTutorial').on('click', function() {
-      var key = $('#tutorialKey').val();
-      var value = $('#tutorialValue').val();
-
-      helper.showSpinner('publishTutorialSpinner');
-
-      storage.putData('tutorial', key, value, function(error) {
-        if (!error) {
-          $('#tutorialValue').val('');
-        }
-
-        helper.hideSpinner('publishTutorialSpinner');
-      });
-
-      return false;
-    });
-
-    $('#fetchTutorialKey').on('click', function() {
-      var key = $('#tutorialKey').val();
-
-      helper.showSpinner('fetchTutorialSpinner');
-
-      storage.getData('tutorial', key, function(error, data) {
-        if(!error && data !== "null") {
-          $('#tutorialValue').val(data);
-        }
-
-        helper.hideSpinner('fetchTutorialSpinner');
-      });
-
-      return false;
-    });*/
 
     $('#disconnect').on('click', function() {
       helper.disconnect();
