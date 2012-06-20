27c04e008ce51e62f971ee1b262b3c4df4a90c8a
Jan-Christoph Borchardt <hey@jancborchardt.net>
2012-06-19 18:42:47 -0700

    add token.js, fix issue #15

---
+++ src/token.js
@@ -0,0 +1,9 @@
+(function() {
+  // `receiveToken` parses the OAuth token from the URL params
+  var token = remoteStorage.receiveToken();
+
+  // We send the token [back to our main page](tutorial.html)
+  window.opener.postMessage(token, location.href);
+  // and close the window.
+  window.close();
+})();
